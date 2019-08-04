ab898c889b40a3110e76f364f43624dda05ece9a
Nick A. Nichols <nichols1991@gmail.com>
2019-08-04 13:18:51 -0500

    Upgrade to clojure 1.10.0

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject brew-bot "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A quick, dirty way to get randomized beer recipes"
+  :url "https://github.com/nnichols/brew-bot"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]]
   :main ^:skip-aot brew-bot.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
