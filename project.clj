df7965d220e87b5160b7daefbd476e4214257360
Nick Nichols <nick.nichols@rate.com>
2019-10-02 15:44:21 -0500

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject brew-bot "0.0.0"
+(defproject brew-bot "1.0.0-alpha"
             :description "A quick, dirty way to get randomized beer recipes"
             :url "https://github.com/nnichols/brew-bot"
             :license {:name "Eclipse Public License"
