8f80d34101417d3dfd74c677ba40a433e4188bc3
Nick Nichols <nick.nichols@rate.com>
2019-11-29 15:42:04 -0600

    Add main backend ns for educational lab

---
+++ src/clj/brew_bot/main.clj
@@ -0,0 +1,9 @@
+(ns brew-bot.main
+  (:require [brew-bot.generators :as generators]
+            [brew-bot.ingredients :as ingredients]
+            [brew-bot.weights :as weights])
+  (:gen-class))
+
+(defn -main
+  [& args]
+  (println "I don't do very much, yet..."))
