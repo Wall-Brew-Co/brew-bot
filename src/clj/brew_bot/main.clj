6e6028f2cf86d515fd85a7a49a11e50e21d1db43
Nick Nichols <nick.nichols@rate.com>
2019-12-05 08:13:42 -0600

    Base for GR demo

--- src/clj/brew_bot/main.clj
+++ src/clj/brew_bot/main.clj
@@ -1,9 +1,17 @@
 (ns brew-bot.main
   (:require [brew-bot.generators :as generators]
             [brew-bot.ingredients :as ingredients]
-            [brew-bot.weights :as weights])
+            [brew-bot.weights :as weights]
+            [nnichols.util :as nu])
   (:gen-class))
 
 (defn -main
   [& args]
   (println "I don't do very much, yet..."))
+
+(nu/rand-kv ingredients/grains)
+(nu/rand-kv ingredients/extracts)
+(nu/rand-kv ingredients/hops)
+(nu/rand-kv ingredients/yeasts)
+
+(generators/generate-beer-recipe :random 5 {:weight 5} {:weight 3.5} {:weight 1.5} {})
