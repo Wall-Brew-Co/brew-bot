94869f68e705beba3e614f52beeb5496371e959b
Nick Nichols <nick.nichols@rate.com>
2019-12-05 08:49:28 -0600

    Remove REPL code from demo

--- src/clj/brew_bot/main.clj
+++ src/clj/brew_bot/main.clj
@@ -4,14 +4,3 @@
             [brew-bot.weights :as weights]
             [nnichols.util :as nu])
   (:gen-class))
-
-(defn -main
-  [& args]
-  (println "I don't do very much, yet..."))
-
-(nu/rand-kv ingredients/grains)
-(nu/rand-kv ingredients/extracts)
-(nu/rand-kv ingredients/hops)
-(nu/rand-kv ingredients/yeasts)
-
-(generators/generate-beer-recipe :random 5 {:weight 5} {:weight 3.5} {:weight 1.5} {})
