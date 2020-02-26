575008f0e0c1156b54b76da0941ef399a0b10995
Nick A. Nichols <nichols1991@gmail.com>
2020-02-25 19:14:11 -0600

    Fix conflict

--- test/brew_bot/runner.cljs
+++ test/brew_bot/runner.cljs
@@ -3,19 +3,11 @@
             [brew-bot.recipe-generation-test]
             [brew-bot.ingredient-test]
             [brew-bot.ingredient-lists.yeasts-test]
-<<<<<<< HEAD
             [brew-bot.style-lists.weight-test]
             [brew-bot.style-lists.pilsner-test]))
-=======
-            [brew-bot.weight-test]))
->>>>>>> master
 
 (doo-tests 'brew-bot.recipe-generation-test
            'brew-bot.ingredient-test
            'brew-bot.ingredient-lists.yeasts-test
-<<<<<<< HEAD
            'brew-bot.style-lists.weight-test
            'brew-bot.style-lists.pilsner-test)
-=======
-           'brew-bot.weight-test)
->>>>>>> master
