31a5ec143d49920b16e77c7cfda8da2c05fabb99
Nick A Nichols <nichols1991@gmail.com>
2020-06-17 12:21:46 -0500

    Update project.clj
    Bump dependencies

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[cljx-sampling "0.1.0"]
-                           [nnichols "0.7.0"]
+                           [nnichols "0.9.0"]
                            [org.clojure/clojure "1.10.1"]
                            [org.clojure/clojurescript "1.10.597" :scope "provided"]]
 
-            :plugins [[lein-cljsbuild "1.1.7"]]
+            :plugins [[lein-cljsbuild "1.1.8"]]
 
             :aliases {"test-build" ["do" "clean" ["cljsbuild" "once" "test"] ["doo" "once"] ["test"]]}
 
