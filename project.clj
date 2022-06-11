29eb1504bd23f8e2304b2bc276f50153a74b15df
WallBrewBot <the.wall.brew@gmail.com>
2022-06-11 15:13:48 +0000

    Bump org.clojure/clojurescript from 1.10.879 to 1.11.57

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  [com.wallbrew/common-beer-format "1.3.1"]
                  [nnichols "1.0.0"]
                  [org.clojure/clojure "1.11.1"]
-                 [org.clojure/clojurescript "1.10.879" :scope "provided"]]
+                 [org.clojure/clojurescript "1.11.57" :scope "provided"]]
 
   :plugins [[lein-cljsbuild "1.1.8"]]
 
