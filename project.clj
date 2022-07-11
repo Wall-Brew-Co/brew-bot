37f964fa483f0977a03278770c5d366136ec5760
WallBrewBot <the.wall.brew@gmail.com>
2022-07-11 23:40:54 +0000

    Bumped com.wallbrew/common-beer-data from 1.0.0 to 1.1.0.
    Inspect dependency changes here: https://github.com/Wall-Brew-Co/common-beer-data/compare/v1.0.0...v1.1.0

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url  "https://opensource.org/licenses/MIT"}
   :dependencies [[cljx-sampling "0.1.0"]
                  [com.wallbrew/brewtility "1.2.0"]
-                 [com.wallbrew/common-beer-data "1.0.0"]
+                 [com.wallbrew/common-beer-data "1.1.0"]
                  [com.wallbrew/common-beer-format "2.0.0"]
                  [nnichols "1.0.0"]
                  [org.clojure/clojure "1.11.1"]
