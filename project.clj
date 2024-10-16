ccffe34dfba7372c68387a792a69449b364e1ace
Nick Nichols <nichols1991@gmail.com>
2024-10-16 22:52:28 +0000

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.wallbrew/brew-bot "3.3.0"
+(defproject com.wallbrew/brew-bot "3.4.0"
   :description "A library to generate randomized beer recipes."
   :url "https://github.com/Wall-Brew-Co/brew-bot"
   :license {:name         "MIT"
